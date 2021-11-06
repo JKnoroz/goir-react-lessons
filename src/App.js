14489e103b0c9cd46ffbf6f5c159016e74b97829
Yuliya <jknoroz@gmail.com>
2021-11-06 21:17:23 +0200

    les2

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Profile from './components/social-profile/profile';
 import user from './data/user.json';
@@ -16,27 +16,49 @@ import Counter from './components/Counter/Counter';
 import Dropdown from './components/Dropdown/Dropdown';
 import ColorPicker from './components/ColorPicker/ColorPicker';
 
+import TodoList from './components/TodoList';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Dropdown />
-      <Profile
-        name={user.name}
-        tag={user.tag}
-        location={user.location}
-        avatar={user.avatar}
-        stats={user.stats}
-      />
-      <Statistics title="Upload stats" stats={statisticalData} />
-      <Statistics stats={statisticalData} />
-      <FriendList friends={friends} />
-      <TransactionHistory items={transactions} />
-      <Counter initialValue={10} />
-      <ColorPicker />
-    </div>
-  );
+const colorPickerOptions = [
+  { label: 'red', color: '#F44336' },
+  { label: 'green', color: '#4CAF50' },
+  { label: 'blue', color: '#2196F3' },
+  { label: 'grey', color: '#607D8B' },
+  { label: 'pink', color: '#E91E63' },
+  { label: 'indigo', color: '#3F51B5' },
+];
+
+class App extends Component {
+  state = {
+    todos: [
+      { id: 'id-1', text: 'Выучить основы React', completed: true },
+      { id: 'id-2', text: 'Разобраться с React Router', completed: false },
+      { id: 'id-3', text: 'Пережить Redux', completed: false },
+    ],
+  };
+  render() {
+    const { todos } = this.state;
+    return (
+      <div className="App">
+        <Dropdown />
+        <Profile
+          name={user.name}
+          tag={user.tag}
+          location={user.location}
+          avatar={user.avatar}
+          stats={user.stats}
+        />
+        <Statistics title="Upload stats" stats={statisticalData} />
+        <Statistics stats={statisticalData} />
+        <FriendList friends={friends} />
+        <TransactionHistory items={transactions} />
+        <Counter initialValue={10} />
+        <ColorPicker options={colorPickerOptions} />
+        <TodoList todos={todos} />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/TodoList/TodoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const TodoList = ({ todos }) => (
+  <ul>
+    {todos.map(({ id, text }) => (
+      <li key={id}>
+        <p>{text}</p>
+      </li>
+    ))}
+  </ul>
+);
+
+export default TodoList;
